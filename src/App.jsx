3be8a9bb546af5d9878918e513e3ce4d25b575b4
Kauan Hindlmayer <kauanhindlmayer07@gmail.com>
2022-09-22 15:00:20 -0300

    properties

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import { Post } from './Post';
 export function App() {
   return (
     <div>
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
+      <Post
+        author="Kauan Hindlmayer"
+        content="Lorem ipsum dolor sit amet consectetur adipisicing elit." 
+      />
+      <Post
+        author="Diego Fernandes"
+        content="Lorem ipsum dolor sit amet consectetur adipisicing elit." 
+      />
     </div>
   )
 }
